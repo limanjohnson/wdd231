01b0c21fb6da37c1703adf481653083287162325
limanjohnson <liamjnell@gmail.com>
2025-01-14 15:48:54 -0700

    LN WDD231 Homepage 1.8 Add Open Graph Meta Properties

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,6 +1,16 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView:1,
-    spaceBetween:30,
-    effect: "fade",
-    loop:true,
+// Initialize Swiper
+const swiper = new Swiper('.mySwiper', {
+    loop: true, // Enable looping
+    navigation: {
+        nextEl: '.swiper-button-next', // Add next button
+        prevEl: '.swiper-button-prev', // Add prev button
+    },
+    pagination: {
+        el: '.swiper-pagination', // Add pagination
+        clickable: true, // Make pagination clickable
+    },
+    autoplay: {
+        delay: 3000, // Set autoplay delay (3 seconds)
+        disableOnInteraction: false, // Keep autoplay after user interacts
+    },
 });
