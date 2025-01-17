8fcd86fc082496bb0a51664cfd52e0570d765e5a
limanjohnson <liamjnell@gmail.com>
2025-01-17 14:50:48 -0700

    LN WDD231 Prophets Activity 1.13

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,40 +1,43 @@
 // Fetch Prophet Information
 
 // const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-const cards = document.querySelector('#cards');
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelector('#cards');
 
-async function getProphetData() {
-    const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.table(data.prophets);
-    displayProphets(data.prophets)
+    async function getProphetData() {
+        const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.table(data.prophets);
+        displayProphets(data.prophets)
 
-}
+    }
 
-getProphetData()
+    getProphetData()
 
-const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
-        let card = document.createElement('section');
-        let fullName = document.createElement('h2');
-        let portrait = document.createElement('img');
+    const displayProphets = (prophets) => {
+        prophets.forEach((prophet) => {
+            let card = document.createElement('section');
+            let fullName = document.createElement('h2');
+            let portrait = document.createElement('img');
 
-        fullName.innerText = `${prophet.name}`
+            fullName.innerText = `${prophet.name} ${prophet.lastname}`
 
-        portrait.setAttribute('src', `${prophet.imageurl}`);
-        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '300');
-        portrait.setAttribute('height', '400');
+            portrait.setAttribute('src', `${prophet.imageurl}`);
+            portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+            portrait.setAttribute('loading', 'lazy');
+            portrait.setAttribute('width', '300');
+            portrait.setAttribute('height', '400');
 
-        card.appendChild(fullName);
-        card.appendChild(portrait);
+            card.appendChild(fullName);
+            card.appendChild(portrait);
 
-        cards.appendChild(card);
+            cards.appendChild(card);
 
-    })
-}
+        })
+    }
 
 
-displayProphets();
+    displayProphets(prophet);
+});
+
