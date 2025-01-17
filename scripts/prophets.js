148c3868bd1446026a7258f932b19f2da65e007a
limanjohnson <liamjnell@gmail.com>
2025-01-17 14:14:11 -0700

    LN WDD231 Prophets Activity 1.1

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -6,8 +6,31 @@ const cards = document.getElementById('#cards');
 async function getProphetDate() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
+    // console.table(data.prophets);
+    displayProphets(data.prophets)
 
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('div');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        fullName.innerText = `${prophet.name}`
+
+        portrait.setAttribute('src', `${imageurl}`);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '200');
+        portrait.setAttribute('height', '200');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+
+    })
 }
 
 getProphetDate();
