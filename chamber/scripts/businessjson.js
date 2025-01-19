485630af1c35afed1ffc34599192d730b5019ada
limanjohnson <liamjnell@gmail.com>
2025-01-19 03:35:56 -0700

    LN WDD231 LandingPage

--- chamber/scripts/businessjson.js
+++ chamber/scripts/businessjson.js
@@ -9,37 +9,46 @@ document.addEventListener('DOMContentLoaded', () => {
         const response = await fetch(source);
         const businessData = await response.json();
         console.table(businessData);
-
+        return businessData;
     }
 
-    getBusinessData()
-
-    const displayBusiness = (businesses) => {
+    const displayBusiness = (businessInformation) => {
         const cards = document.querySelector('#businessCard');
         cards.innerHTML = '';
-        businesses.forEach((business) => {
+        businessInformation.forEach((business) => {
             let businessCard = document.createElement('section');
-            let businessName = document.createElement('h2');
             let businessLogo = document.createElement('img');
+            let businessName = document.createElement('h2');
+
 
             businessName.innerText = `${business.businessName}`
 
-            businessLogo.setAttribute('src', `${business.images}`);
-            businessLogo.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
-            businessLogo.setAttribute('loading', 'lazy');
-            businessLogo.setAttribute('width', '300');
-            businessLogo.setAttribute('height', '400');
+            businessLogo.setAttribute('src', `${business.images}`)
 
-            businessCard.appendChild(businessName);
             businessCard.appendChild(businessLogo);
+            businessCard.appendChild(businessName);
             cards.appendChild(businessCard);
 
         })
+
+        // businessInformation.forEach((business) => {
+        //     let businessList = document.createElement('section')
+        // })
     }
 
 
+    // Fetch business data and pass it to displaybusiness
+    getBusinessData().then((businessData) => {
+        if (businessData && businessData.businessInformation) {
+        displayBusiness(businessData.businessInformation);
+        } else {
+            console.error('no valid data found', businessData)
+        }
+    });
 
-    displayBusiness();
+    // Display list vs grid
+    const gridbutton = document.querySelector("#grid");
+    const listbutton = document.querySelector("#list");
 
 });
 
