16059378195ded9879cffa57d227f945eabba7e7
limanjohnson <liamjnell@gmail.com>
2025-01-22 10:35:18 -0700

    LN Chamber of Commerce Home Page
    Use modules

--- chamber/scripts/homepage.js
+++ chamber/scripts/homepage.js
@@ -1,3 +1,8 @@
+import setupNavigationMenu from './navigateMenu.js';
 import displayWeather from './getweather.js';
+import getDate from './last-modified.js';
+
+setupNavigationMenu();
 
 document.addEventListener('DOMContentLoaded', displayWeather);
+getDate();
--- chamber/scripts/last-modified.js
+++ chamber/scripts/last-modified.js
@@ -1,11 +1,12 @@
 // Find the last modified date & time
+export default function getDate() {
+    let oLastModif = new Date(document.lastModified);
 
-let oLastModif = new Date(document.lastModified);
+    let formatDate = new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
 
-let formatDate = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-});
-
-document.getElementById("lastModified").innerHTML = formatDate.format(oLastModif);
+    document.getElementById("lastModified").innerHTML = formatDate.format(oLastModif);
+}
