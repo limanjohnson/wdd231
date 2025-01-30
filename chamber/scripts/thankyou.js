97d8b4893d93b6b6964bf30222332d6414dab075
limanjohnson <liamjnell@gmail.com>
2025-01-30 00:45:55 -0700

    LN Chamber of Commerce Home Page
    Join and Thank You page

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,7 +1,5 @@
 import getDate from './lastModified.js';
 import setupNavigationMenu from './navigateMenu.js';
-import getSubmittedInfo from "./getSubmittedInformation";
 
 setupNavigationMenu()
 getDate();
-getSubmittedInfo();
