9fe47ef148be09183859a588620fe6b55cede7fc
limanjohnson <liamjnell@gmail.com>
2025-02-15 14:18:37 -0700

    LN LeadVault
    Fixed View Leads logic

--- LeadVault/scripts/general.js
+++ LeadVault/scripts/general.js
@@ -11,3 +11,23 @@ navItems.forEach(item => {
         item.classList.add('active');
     })
 })
+
+// Hamburger Menu
+const menu = document.getElementById("menu");
+
+const input = document.createElement("input");
+const label = document.createElement("checkbox");
+const div = document.createElement("div");
+const span = document.createElement("span");
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.querySelector("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    // document.querySelector("main").style.marginLeft = "0";
+}
+
